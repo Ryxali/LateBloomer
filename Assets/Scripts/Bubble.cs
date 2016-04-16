5987bb61786d3bca9502a0bb1b4a10d0bf07ddc9
Niklas Lindblad <ryxali@gmail.com>
2016-04-16 12:55:42 +0200

    Added bubble effect

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(CircleCollider2D))]
+public class Bubble : MonoBehaviour {
+    public BubbleEffect bubbleEffectPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TRIGGER");
+        Caterpillar c = other.GetComponent<Caterpillar>();
+        if(c != null)
+        {
+            BubbleEffect effect = Instantiate<BubbleEffect>(bubbleEffectPrefab);
+            effect.transform.parent = c.transform;
+            effect.transform.localPosition = Vector3.zero;
+            effect.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/BubbleEffect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleEffect : MonoBehaviour {
+    public float effectMagnitude = 15.0f;
+    public float duration = 2.0f;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(EffectSequence());
+	}
+
+    IEnumerator EffectSequence()
+    {
+        Caterpillar caterpillar = GetComponentInParent<Caterpillar>();
+        caterpillar.acceleration += Vector3.up * effectMagnitude;
+        yield return new WaitForSeconds(duration);
+        caterpillar.acceleration -= Vector3.up * effectMagnitude;
+        Destroy(gameObject);
+    }
+}
